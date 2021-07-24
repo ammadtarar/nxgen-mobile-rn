73eb493c184475429d6a51dd78a0bdec8fcf658f
Ammad Amjad Tarar <info@sawatechnologies.org>
2021-07-24 20:52:43 +0500

    Added navigatio

--- Screens/Home.js
+++ Screens/Home.js
@@ -11,33 +11,31 @@ import Settings from './Settings'
 const Tab = createBottomTabNavigator();
 export default function App() {
     return (
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
-                        if (route.name === 'Reports') {
-                            iconName = focused ? 'ios-file-tray-stacked' : 'ios-file-tray-stacked-outline';
-                        } else if (route.name === 'Appointments') {
-                            iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
-                        } else if (route.name === 'Rate List') {
-                            iconName = focused ? 'md-pricetag' : 'md-pricetag-outline';
-                        } else if (route.name === 'Settings') {
-                            iconName = focused ? 'ios-settings' : 'ios-settings-outline';
-                        }
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                })}
-                tabBarOptions={{
-                    activeTintColor: 'orange',
-                    inactiveTintColor: 'gray',
-                }}
-            >
-                <Tab.Screen name="Reports" component={Reports} />
-                <Tab.Screen name="Appointments" component={Appointments} />
-                <Tab.Screen name="Rate List" component={RateList} />
-                <Tab.Screen name="Settings" component={Settings} />
-            </Tab.Navigator>
-        </NavigationContainer>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Reports') {
+                        iconName = focused ? 'ios-file-tray-stacked' : 'ios-file-tray-stacked-outline';
+                    } else if (route.name === 'Appointments') {
+                        iconName = focused ? 'ios-calendar' : 'ios-calendar-outline';
+                    } else if (route.name === 'Rate List') {
+                        iconName = focused ? 'md-pricetag' : 'md-pricetag-outline';
+                    } else if (route.name === 'Settings') {
+                        iconName = focused ? 'ios-settings' : 'ios-settings-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'orange',
+                inactiveTintColor: 'gray',
+            }}
+        >
+            <Tab.Screen name="Reports" component={Reports} />
+            <Tab.Screen name="Appointments" component={Appointments} />
+            <Tab.Screen name="Rate List" component={RateList} />
+            <Tab.Screen name="Settings" component={Settings} />
+        </Tab.Navigator>
     );
 }
--- index.js
+++ index.js
@@ -7,4 +7,4 @@ import Home from './Screens/Home';
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Home);
+registerRootComponent(App);
