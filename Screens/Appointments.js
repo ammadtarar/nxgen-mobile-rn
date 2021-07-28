546d6833713a561cb6ae959931a6f331bf449f88
Ammad Amjad Tarar <info@sawatechnologies.org>
2021-07-28 18:58:02 +0500

    Finsihed UI for the appointmetns list

--- Screens/Appointments.js
+++ Screens/Appointments.js
@@ -1,21 +1,236 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const appointmentsData = [
+    {
+        id: 0,
+        patient: "Ammad Amjad",
+        test_name: "Blood Test",
+        type: "Home Pickup",
+        date: "13 July 2021",
+        status: "pending",
+    },
+    {
+        id: 0,
+        patient: "Ammad Amjad",
+        test_name: "Blood Test",
+        type: "Home Pickup",
+        date: "13 July 2021",
+        status: "completed",
+    },
+    {
+        id: 0,
+        patient: "Ammad Amjad",
+        test_name: "Blood Test",
+        type: "Home Pickup",
+        date: "13 July 2021",
+        status: "cancelled",
+    },
+    {
+        id: 0,
+        patient: "Ammad Amjad",
+        test_name: "Blood Test",
+        type: "Home Pickup",
+        date: "13 July 2021",
+        status: "pending",
+    },
+    {
+        id: 0,
+        patient: "Ammad Amjad",
+        test_name: "Blood Test",
+        type: "Home Pickup",
+        date: "13 July 2021",
+        status: "pending",
+    },
+];
 
 export default function App() {
+
+    const [appointments, setAppointments] = useState(appointmentsData);
+
+    function getStatusClass(status) {
+        var color = ''
+        if (status == 'pending') {
+            color = 'orange';
+        } else if (status == 'completed') {
+            color = 'green';
+        } else if (status == 'cancelled') {
+            color = 'red';
+        } else {
+            color = 'black';
+        }
+        return {
+            color: color,
+            fontWeight: '600'
+        };
+    }
+    const renderItem = ({ item }) => (
+        <View style={s.item}>
+            <View style={s.top}>
+                <Text style={s.itemTitle}>{item.patient}</Text>
+                <View style={s.itemDateWrapper}>
+                    <Ionicons size={16} name='ios-calendar-sharp' color='black' />
+                    <Text style={s.itemDate}>{item.date}</Text>
+                </View>
+            </View>
+
+            <View style={s.bottom}>
+                <View style={s.bottomItem}>
+                    <Text style={s.bottomItemTitle}>Status</Text>
+                    <Text style={[s.bottomItemValue, getStatusClass(item.status)]}>{item.status}</Text>
+                </View>
+                <View style={s.hSpacer}></View>
+                <View style={s.bottomItem}>
+                    <Text style={s.bottomItemTitle}>Test Name</Text>
+                    <Text style={s.bottomItemValue}>{item.test_name}</Text>
+                </View>
+                <View style={s.hSpacer}></View>
+                <View style={s.bottomItem}>
+                    <Text style={s.bottomItemTitle}>Type</Text>
+                    <Text style={s.bottomItemValue}>{item.type}</Text>
+                </View>
+            </View>
+
+
+            {
+                item.status == 'pending' &&
+                <View style={s.itemActions}>
+                    <TouchableOpacity>
+                        <View style={s.itemAction}>
+                            <Ionicons size={18} name='ios-close' color='white' />
+                            <Text style={s.itemActionText}>Cancel</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            }
+
+        </View >
+    );
+
+
     return (
-        <View style={styles.container}>
-            <Text>Appointments Screen</Text>
+        <View style={s.container}>
+
+            <View style={s.titleContainer}>
+                <Text style={s.pageTitle}>Appointments</Text>
+                <TouchableOpacity >
+                    <Ionicons size={30} name='md-add-circle-sharp' color='orange' />
+                </TouchableOpacity>
+            </View>
+            <FlatList
+                data={appointments}
+                renderItem={renderItem}
+                key={item => item.id}
+            />
+
             <StatusBar style="auto" />
         </View>
     );
 }
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
+    titleContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingTop: 14,
+        paddingBottom: 10,
+        paddingLeft: 14,
+        paddingRight: 14
+    },
+    pageTitle: {
+        fontSize: 20,
+        fontWeight: '800'
+    },
+    item: {
+        padding: 20,
+        backgroundColor: 'white',
+        borderRadius: 4,
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10
+    },
+    top: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    itemTitle: {
+        fontSize: 14,
+        color: 'black',
+        fontWeight: '600'
+    },
+
+    itemDateWrapper: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 'auto'
+    },
+    itemDate: {
+        fontSize: 12,
+        marginLeft: 4
+    },
+    bottom: {
+        display: 'flex',
+        flexDirection: 'row',
+        marginTop: 16,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    bottomItem: {
+        width: '33%',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    bottomItemTitle: {
+        color: 'gray',
+        fontSize: 10,
+        textTransform: 'uppercase',
+        fontWeight: '600',
+        textAlign: 'center'
+    },
+    bottomItemValue: {
+        color: 'black',
+        fontSize: 12,
+        marginTop: 8,
+        textTransform: 'capitalize'
+    },
+    hSpacer: {
+        width: 1,
+        height: 20,
+        marginLeft: 4,
+        marginRight: 4,
+        backgroundColor: 'lightgray'
+    },
+    itemActions: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        marginLeft: 'auto',
+        marginTop: 20,
+    },
+    itemAction: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#EF5350',
+        paddingTop: 6,
+        paddingBottom: 6,
+        paddingLeft: 10,
+        paddingRight: 10,
+        borderRadius: 20
+    },
+    itemActionText: {
+        fontSize: 12,
+        color: 'white',
+        marginLeft: 4,
+        fontWeight: '600',
+        marginRight: 4
+    }
 });
